bb793ada96aa1ee17c2842f5ae61ceca7373b99d
Lukas Siegfried Heinz <lukas.s.heinz@stud.h-da.de>
2022-09-28 20:18:13 +0200

    update Config

--- config.lua
+++ config.lua
@@ -4,6 +4,53 @@ Config.Locale  = 'de'
 Config.HuntingWeapon = "WEAPON_MUSKET"
 Config.HuntingAreaRange = 460.0
 
+Config.HunntingAnimals = {
+    [0] = {
+        ['model'] = "a_c_boar"
+        ['probability'] = 0.1, --  = 10%
+    }
+    [1] = {
+        ['model'] = "a_c_deer"
+        ['probability'] = 0.3, --  = 30%
+    }
+    [2] = {
+        ['model'] = "a_c_coyote"
+        ['probability'] = 0.3, --  = 30%
+    }
+    [3] = {
+        ['model'] = "a_c_husky"
+        ['probability'] = 0.3, --  = 30%
+    }
+   [4] = {
+    ['model'] = "a_c_pig"
+    ['probability'] = 0.3, --  = 30%
+   }
+   [5] = {
+    ['model'] = "a_c_pig"
+    ['probability'] = 0.3, --  = 30%
+   }
+   [6] = {
+    ['model'] = "a_c_rabbit_01"
+    ['probability'] = 0.3, --  = 30%
+   }
+}
+Cofing.HuntingAreaRanges = {
+    [0] = {
+        ["coord"]= vector3(1,1,1),
+        ["radius"] = 460
+    },
+    [1] = {
+        ["coord"]= vector3(1,1,1),
+        ["radius"] = 460
+    }
+}
+Config.HunntingMensions {
+    [0] = {
+        ["coord"] = vector3(1,3,5),
+        ["actions"] = {"start","sell"}
+    }
+}
+
 Config.Notification = {}
 Config.Notification.System = 'lp_notify' -- none / lp_notify
 Config.Notification.displaytime = 1300 --ms
