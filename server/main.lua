ec20ce32e88ba5c05dbcadb6b0cd27ae5fc142f1
Lukas Heinz <mail@heinz-lukas.de>
2022-09-29 11:39:16 +0000

    Update server/main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,48 +1,8 @@
-------------------------------------| Register ESX |-------------------------------------
+------------------------------------| Variable Declaration |---------------------------------
 ESX = nil
-
+------------------------------------| Initial ESX |------------------------------------------
 TriggerEvent("esx:getSharedObject",function(obj) ESX = obj end)
-
-
-RegisterServerEvent('lp_hunting:reward')
-AddEventHandler('lp_hunting:reward', function(Weight)
-    local xPlayer = ESX.GetPlayerFromId(source)
-
-    if Weight >= 1 then
-        xPlayer.addInventoryItem('meat', 1)
-    elseif Weight >= 9 then
-        xPlayer.addInventoryItem('meat', 2)
-    elseif Weight >= 15 then
-        xPlayer.addInventoryItem('meat', 3)
-    end
-
-    xPlayer.addInventoryItem('leather', math.random(1, 4))
-        
-end)
-
-RegisterServerEvent('lp_hunting:sell')
-AddEventHandler('lp_hunting:sell', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-
-    local MeatPrice    = 400
-    local LeatherPrice = 170
-
-    local MeatQuantity = xPlayer.getInventoryItem('meat').count
-    local LeatherQuantity = xPlayer.getInventoryItem('leather').count
-
-    if MeatQuantity > 0 or LeatherQuantity > 0 then
-        xPlayer.addMoney(MeatQuantity * MeatPrice)
-        xPlayer.addMoney(LeatherQuantity * LeatherPrice)
-
-        xPlayer.removeInventoryItem('meat', MeatQuantity)
-        xPlayer.removeInventoryItem('leather', LeatherQuantity)
-        TriggerClientEvent('esx:showNotification', xPlayer.source, 'Du verkaufst ' .. LeatherQuantity + MeatQuantity .. 'kg und erhältst ~g~' .. LeatherPrice * LeatherQuantity + MeatPrice * MeatQuantity..'$')
-    else
-        TriggerClientEvent('esx:showNotification', xPlayer.source, '~r~Du hast weder Fleisch noch Leder dabei.')
-    end
-        
-end)
-
+------------------------------------| Usfull Functions |-------------------------------------
 function GetCoordZ(x, y)
 	local groundCheckHeights = { 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0, 500.0 }
     local groundCheck = 500.0
@@ -87,12 +47,53 @@ function generateAnimalSpawnLocation(HuntingArea, areaRange)
             print("not found ground coord Z")
             return false
         end
-        else
+    else
         print("player is to far to spawn a Animal")
         return false
     end
 	
 end
+------------------------------------| Register Net Events |-------------------------------------
+RegisterServerEvent('lp_hunting:reward')
+AddEventHandler('lp_hunting:reward', function(Weight)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if Weight >= 1 then
+        xPlayer.addInventoryItem('meat', 1)
+    elseif Weight >= 9 then
+        xPlayer.addInventoryItem('meat', 2)
+    elseif Weight >= 15 then
+        xPlayer.addInventoryItem('meat', 3)
+    end
+
+    xPlayer.addInventoryItem('leather', math.random(1, 4))
+        
+end)
+
+RegisterServerEvent('lp_hunting:sell')
+AddEventHandler('lp_hunting:sell', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    local MeatPrice    = 400
+    local LeatherPrice = 170
+
+    local MeatQuantity = xPlayer.getInventoryItem('meat').count
+    local LeatherQuantity = xPlayer.getInventoryItem('leather').count
+
+    if MeatQuantity > 0 or LeatherQuantity > 0 then
+        xPlayer.addMoney(MeatQuantity * MeatPrice)
+        xPlayer.addMoney(LeatherQuantity * LeatherPrice)
+
+        xPlayer.removeInventoryItem('meat', MeatQuantity)
+        xPlayer.removeInventoryItem('leather', LeatherQuantity)
+        TriggerClientEvent('esx:showNotification', xPlayer.source, 'Du verkaufst ' .. LeatherQuantity + MeatQuantity .. 'kg und erhältst ~g~' .. LeatherPrice * LeatherQuantity + MeatPrice * MeatQuantity..'$')
+    else
+        TriggerClientEvent('esx:showNotification', xPlayer.source, '~r~Du hast weder Fleisch noch Leder dabei.')
+    end
+        
+end)
+
+
 RegisterServerEvent('lp_hunting:spawnPeds')
 AddEventHandler('lp_hunting:spawnPeds', function(nearestHuntingArea)
     local peds = {}
